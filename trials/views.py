954a521c4676ecdf51e96bbee6e5bf34c15dfa5a
ChanHaeng Lee <2chanhaeng@gmail.com>
2022-11-30 14:53:55 +0900

    Create TrialsView view
    최근 7일 동안 업데이트 된 임상시험 정보를 가져오는 view입니다.

--- trials/views.py
+++ trials/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from datetime import datetime, timedelta
+from rest_framework.views import APIView
+from rest_framework.request import Request
+from rest_framework.response import Response
+from .models import Trial
+from .serializers import TrialSerializer
 
-# Create your views here.
+
+class TrialsView(APIView):
+    def get(self, request: Request) -> Response:
+        """
+        Get trials that updated for 7 days.
+        GET /api/v1/trials/
+        """
+
+        trials = Trial.objects.filter(
+            updated_at__gte=datetime.now() - timedelta(days=7)
+        )
+        serializer = TrialSerializer(trials, many=True)
+        return Response(serializer.data)

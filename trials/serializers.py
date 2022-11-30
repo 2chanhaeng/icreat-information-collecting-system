6e373b84fcbaa137ed7ae031fa1f6abde639e76d
ChanHaeng Lee <2chanhaeng@gmail.com>
2022-11-30 14:30:17 +0900

    Create TrialSerializer
    `Trial` 모델의 serializer 입니다.

---
+++ trials/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from .models import Trial
+
+
+class TrialSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Trial
+        fields = "__all__"

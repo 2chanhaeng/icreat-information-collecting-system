4cfc398caf6b67375feabb4c40fc00aad80c07b3
ChanHaeng Lee <2chanhaeng@gmail.com>
2022-11-30 15:01:59 +0900

    Create TrialView view
    과제번호(Trial.number)로 특정 임상시험 정보를 조회하는 view입니다.

--- trials/views.py
+++ trials/views.py
@@ -4,6 +4,7 @@ from rest_framework.request import Request
 from rest_framework.response import Response
 from .models import Trial
 from .serializers import TrialSerializer
+from rest_framework.status import HTTP_404_NOT_FOUND
 
 
 class TrialsView(APIView):
@@ -17,4 +18,19 @@ class TrialsView(APIView):
             updated_at__gte=datetime.now() - timedelta(days=7)
         )
         serializer = TrialSerializer(trials, many=True)
+        return Response(serializer.data)
+
+
+class TrialView(APIView):
+    def get(self, request: Request, pk: str) -> Response:
+        """
+        Get a trial by number.
+        GET /api/v1/trials/{number}
+        """
+
+        try:
+            trial = Trial.objects.get(number=pk)
+        except Trial.DoesNotExist:
+            return Response({"message": "Not found"}, status=HTTP_404_NOT_FOUND)
+        serializer = TrialSerializer(trial)
         return Response(serializer.data)
